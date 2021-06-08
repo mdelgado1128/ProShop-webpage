e080c82c6518b592d0b5c194d1cb4f27ada4d847
Mark Delgado <markdelgado94@gmail.com>
2021-06-07 23:45:17 -0700

    database schema created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
-const express = require("express")
-const products = require("./data/products")
-const app = express()
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
+import products from "./data/products.js"
+import colors from "colors"
 
+dotenv.config()
+connectDB()
+
+const app = express()
 app.get("/", (req, res) => {
   res.send("Api is running")
 })
@@ -14,4 +20,13 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
-app.listen(5000, console.log("Server runnig on port 5000"))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server runnig on port ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+)

4cb75c01bc931d7a4e02d7578b94c26b3cf0987d
Mark Delgado <markdelgado94@gmail.com>
2021-07-22 23:15:41 -0700

    shopping cart page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer.js"
-import HomeScreen from "./screens/HomeScreen"
 import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
+import CartScreen from "./screens/CartScreen"
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />

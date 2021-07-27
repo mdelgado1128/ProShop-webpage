9b178f1bf205269a702b0ba0a2babece3224b14a
Mark Delgado <markdelgado94@gmail.com>
2021-07-26 22:30:23 -0700

    backend routes reorganized with controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,8 @@
+import asyncHandler from "express-async-handler"
+import User from "../models/userModel.js"
+
+//**@desc Auth user and get token
+//**@route POST /api/users/login
+//**@access Public
+
+const authUser = asyncHandler(async (req, res) => {})

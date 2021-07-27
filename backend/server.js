4ce856558afb55b2800d9df296ecf6f30d8c4e7f
Mark Delgado <markdelgado94@gmail.com>
2021-07-27 16:00:10 -0700

    backend authentication controller, middleware, and route created

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,22 @@ import colors from "colors"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config()
+
 connectDB()
 
 const app = express()
+
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Api is running")
 })
 
 app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
 
 app.use(notFound)
 

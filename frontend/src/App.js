eead43dc06535b4a20dd8a6a5390c3ed318e6a1f
Mark Delgado <markdelgado94@gmail.com>
2021-08-07 00:05:05 -0700

    constant, reducer, action, payment screen, and shipping screen implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import LoginScreen from "./screens/LoginScreen"
 import RegisterScreen from "./screens/RegisterScreen"
 import ProfileScreen from "./screens/ProfileScreen"
 import ShippingScreen from "./screens/ShippingScreen"
+import PaymentScreen from "./screens/PaymentScreen"
+import PlaceOrderScreen from "./screens/PlaceOrderScreen"
 
 const App = () => {
   return (
@@ -18,8 +20,10 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/login' component={LoginScreen} exact />
           <Route path='/shipping' component={ShippingScreen} exact />
+          <Route path='/payment' component={PaymentScreen} exact />
+          <Route path='/placeorder' component={PlaceOrderScreen} exact />
+          <Route path='/login' component={LoginScreen} exact />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
